3ee890966c3ba5e573f17158a1acb59356459845
Carlos André Lima de Matos <carlosandretaiga@gmail.com>
2022-10-05 18:20:36 -0300

    feat: added schema auth

---
+++ src/schemas/authSchema.ts
@@ -0,0 +1,17 @@
+import joi from 'joi';
+import { CreateUserData } from '../interfaces/authInterfaces';
+
+type CreateUserDataSignin = Omit<CreateUserData, "name" | "confirmPassword">;
+
+export const signupSchema = joi.object<CreateUserData>({
+  name: joi.string().max(20).required(),
+  email: joi.string().email().required(),
+  password: joi.string().min(4).pattern(new RegExp("^[a-zA-Z0-9]{10,}$")).required(),
+  confirmPassword: joi.ref('password'),
+});
+
+export const signinSchema = joi.object<CreateUserDataSignin>({
+  email: joi.string().email().required(),
+  password: joi.string().required(),
+
+})
--- src/server.ts
+++ src/server.ts
@@ -4,4 +4,4 @@ import app from './app.js';
 const PORT = +process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(chalk.bold.green(`Server run on port ${PORT}!`));
-})
+});
