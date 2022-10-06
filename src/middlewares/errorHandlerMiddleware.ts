e02830ce706a40b746282fa70eca2ef36969a55d
Carlos André Lima de Matos <carlosandretaiga@gmail.com>
2022-10-05 22:44:48 -0300

    feat: added middleware error handler

---
+++ src/middlewares/errorHandlerMiddleware.ts
@@ -0,0 +1,23 @@
+import { Request, Response, NextFunction } from 'express';
+import chalk from 'chalk';
+import { errorTypeToStatusCode, isAppError } from '../utils/errorHandlerUtils';
+
+export function handleError(
+  error,
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
+  console.log(chalk.bold.yellow("Alert: an error occured!", error));
+
+  if(isAppError(error)) {
+    const statusCode = errorTypeToStatusCode(error.type);
+    res.status(statusCode).send(error.message);
+  };
+
+  if(error.details) {
+    return res.status(422).send(error.details.map(({ message }: {message: string })=> message));
+  };
+
+  res.sendStatus(500);
+};
