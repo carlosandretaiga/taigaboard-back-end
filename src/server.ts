52592f944742259fe01ea8be85cfec0ea607d9b2
Carlos André Lima de Matos <carlosandretaiga@gmail.com>
2022-10-10 21:14:16 -0300

    fix: correcting config PORT

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,8 @@
 import chalk from 'chalk';
 import app  from './app.js';
 
-const PORT = +process.env.PORT || 4000;
+const PORT = process.env.PORT || '4000';
+
 app.listen(PORT, () => {
   console.log(chalk.bold.green(`Server run on port ${PORT}!`));
 });
